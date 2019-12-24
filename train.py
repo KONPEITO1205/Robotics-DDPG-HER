d8a1c9526b4b387756e0ad512d441ddbd019cd14
KONPEITO1205 <b1016036@fun.ac.jp>
2019-12-24 14:19:22 +0900

    [update] for convolutional model

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import os, sys
 from arguments import get_args
 from mpi4py import MPI
 from subprocess import CalledProcessError
-from ddpg_agent import ddpg_agent
+from conv_ddpg_agent import ddpg_agent
 import gym_dobot.envs as envs
 import glfw
 from types import MethodType
@@ -16,7 +16,8 @@ train the agent, the MPI part code is copy from openai baselines(https://github.
 def get_env_params(env):
     obs = env.reset()
     # close the environment
-    params = {'obs': obs['observation'].shape[0],
+    # params = {'obs': obs['observation'].shape[0],
+    params = {'obs': 3,
             'goal': obs['desired_goal'].shape[0],
             'action': env.action_space.shape[0],
             'action_max': env.action_space.high[0],
@@ -43,6 +44,13 @@ def launch(args):
     ddpg_trainer = ddpg_agent(args, env, env_params)
     ddpg_trainer.learn()
 
+
+def test(args):
+    env = env_wrapper(gym.make(args.env_name, rand_dom=1))
+    env_params = get_env_params(env)
+    ddpg_trainer = ddpg_agent(args, env, env_params)
+    _ = ddpg_trainer._eval_agent()
+
 if __name__ == '__main__':
     # take the configuration for the HER
     os.environ['OMP_NUM_THREADS'] = '1'
@@ -51,3 +59,4 @@ if __name__ == '__main__':
     # get the params
     args = get_args()
     launch(args)
+    # test(args)
