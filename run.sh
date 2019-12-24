06329acf2b691af6d3acc1afa9acf38c8c938aca
KONPEITO1205 <b1016036@fun.ac.jp>
2019-12-24 14:28:07 +0900

    [update] to use cuda when learning

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gnome-terminal -- bash -c "/home/mikami-lab/anaconda3/bin/python train.py --env-name DobotPush-v1 --learning-from $1 ; bash"
+gnome-terminal -- bash -c "/home/mikami-lab/anaconda3/bin/python train.py --env-name DobotPush-v1 --learning-from $1 --cuda; bash"
 
