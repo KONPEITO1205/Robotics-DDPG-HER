1677b9f38110820bbbe7577736586689fdd38b95
KONPEITO1205 <b1016036@fun.ac.jp>
2020-01-15 23:12:07 +0900

    [update] noise coeff 0.2 to 0.8

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-gnome-terminal -- bash -c "/home/mikami-lab/anaconda3/bin/python train.py --env-name DobotPush-v1 --learning-from $1 --n-skip-frames 10 --cuda; bash"
+gnome-terminal -- bash -c "/home/mikami-lab/anaconda3/bin/python train.py --env-name DobotPush-v1 --learning-from $1 --cuda --noise-eps 0.8; bash"
 
